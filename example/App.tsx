6e57749c547998159a3b48a27e1d6b7480997a31
daheeahn <deg9810@gmail.com>
2024-05-28 15:06:10 +0900

    chore: add android setting for build (success)

--- example/App.tsx
+++ example/App.tsx
@@ -1,10 +1,5 @@
-import { MyRiveReactNativeView } from "my-rive-react-native";
 import { View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={{ flex: 1 }}>
-      <MyRiveReactNativeView resourceName="teddy" />
-    </View>
-  );
+  return <View style={{ flex: 1, backgroundColor: "skyblue" }} />;
 }
